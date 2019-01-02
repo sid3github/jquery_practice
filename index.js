523dd61352c8cb874358529a6ddde326dda9b2c2
Siddharth <siddharthpadwal3@gmail.com>
2019-01-03 00:07:17 +0530

    added first jquery code to change/hide text content via button

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  // loaded the document
+  $("button").click(function() {
+    // search for button element and added click functionality
+    $("p").hide(); // then searched for para element and made it hidden (all para)
+  });
+  $("a").click(function() {
+    $("h1").hide();
+  });
+});
