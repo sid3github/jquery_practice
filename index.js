24172ff94e18e727f32ce85aad817c5364558861
Siddharth <siddharthpadwal3@gmail.com>
2019-01-04 22:07:47 +0530

    practised jquery toggle hide/show functionality

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-$(document).ready(function() {
-  // loaded the document
-  $("button").click(function() {
-    // search for button element and added click functionality
-    $("p").hide(); // then searched for para element and made it hidden (all para)
+// $(document).ready(function() {
+// //   // loaded the document
+// //   $("button").click(function() {
+// //     // search for button element and added click functionality
+// //     $("p").hide(); // then searched for para element and made it hidden (all para)
+// //   });
+// //   $("a").click(function() {
+// //     $("h1").hide();
+// //   });
+//   $("button").click(function() {
+//     $("p:first").hide();
+//   });
+// });
+
+$(function(){
+$("span").click(function() {
+  $("img").toggle(1000, function(){
+    $(".sample").css("text-align","center");
   });
-  $("a").click(function() {
-    $("h1").hide();
-  });
-  $("button").click(function() {
-    $("p:first").hide();
-  });
+});
 });
